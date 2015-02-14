fea26c7cd7e1b0ea9d031618b89c2f490dfe22fe
gatorbotics <gatorbotics1700@gmail.com>
2015-02-13 22:19:57 -0800

    Calibrate lifter command

---
+++ src/org/usfirst/frc/team1700/robot/commands/CalibrateLifterCommand.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team1700.robot.commands;
+
+import org.usfirst.frc.team1700.robot.Subsystems;
+import org.usfirst.frc.team1700.robot.subsystems.LifterMotorSubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class CalibrateLifterCommand extends Command {
+	private LifterMotorSubsystem lifter;
+
+    public CalibrateLifterCommand() {
+        requires(Subsystems.lifter);
+        lifter = Subsystems.lifter;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	lifter.enable();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	lifter.unsafeMove(-5000 + lifter.getPosition());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	return lifter.limitSwitchHit();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	lifter.zeroEncoders();
+    	lifter.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	lifter.stop();
+    }
+}
